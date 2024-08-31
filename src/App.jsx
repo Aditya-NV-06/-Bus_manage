33d89a66013d10725ccce5edccae511127c3fa82
Hari Dev <hariharidev5@gmail.com>
2024-08-31 16:32:20 +0530

    Dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from "./assets/Navbar.jsx"
+import Navbar from "./Navbar.jsx"
 function App() {
   
 
@@ -14,16 +14,19 @@ function App() {
       <div className="main-container">
         <div className="container container1">
           <h2>Total number of Buses</h2>
-
+            <span>76</span>
         </div>
         <div className=" container container2">
           <h2>No. of Buses Active</h2>
+          <span>76</span>
         </div>
         <div className=" container container3">
           <h2>Routes</h2>
+          <span>76</span>
         </div>
         <div className="container container4">
-          <h2></h2>
+          <h2>Employees</h2>
+          <span>76</span>
         </div>
         </div>
      </main>
---
+++ src/Navbar.jsx
@@ -0,0 +1,28 @@
+
+const Navbar = ()=>{
+    return (
+    <>
+         <header>
+      <div className="top">
+        <div className='navbar'>
+          <div className="nav-title">DTC</div>
+        <nav>
+          <ul className="nav-ul">
+            <div className="nav-item"><li><a href="#">Home</a></li></div>
+            <div className="nav-item"><li ><a href="#">Routes</a></li></div>
+            <div className="nav-item"> <li><a href="#">Ticketing</a></li></div>
+            <div className="nav-item"><li><a href="#">Queries</a></li></div>
+            <div className="nav-item"><li><a href="#">Contact</a></li></div>
+            
+          </ul>
+          </nav>
+          </div>
+         <div className="title">Delhi Transport Commission</div>
+        </div>
+     </header>
+    </>
+
+    );
+}
+
+export default Navbar;
