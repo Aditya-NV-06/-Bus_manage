50f89ca0c895bace653a0dee6d1e225a8dc8ebae
Hari Dev <hariharidev5@gmail.com>
2024-08-31 11:14:11 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <header>
+        <h1>Hello</h1>
+     </header>
+     <main>
+      
+     </main>
+     <footer>
+
+     </footer>
+
+    </>
+  )
+}
+
+export default App
