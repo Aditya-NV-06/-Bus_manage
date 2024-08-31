4d386efc20ee7b7a379a3964af82e851201d932c
Hari Dev <hariharidev5@gmail.com>
2024-08-31 15:09:46 +0530

    main page modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navbar from "./assets/Navbar.jsx"
 function App() {
   
 
   return (
     <>
-     <header>
-        <nav className="navbar">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Routes</a></li>
-            <li><a href="#">Ticketing</a></li>
-            <li><a href="#">Queries</a></li>
-            <li><a href="#">Contact</a></li>
-            
-          </ul>
-          <button className="nav-login">Login</button>
-        </nav>
-     </header>
 
 
+     <Navbar/>
 
      <main>
-        <div className="container1">
+      <div className="main-container">
+        <div className="container container1">
           <h2>Total number of Buses</h2>
 
         </div>
-        <div className="container2">
+        <div className=" container container2">
+          <h2>No. of Buses Active</h2>
+        </div>
+        <div className=" container container3">
+          <h2>Routes</h2>
+        </div>
+        <div className="container container4">
           <h2></h2>
         </div>
-        <div className="container3">
-
-        </div>
-        <div className="container4">
-
         </div>
      </main>
 
