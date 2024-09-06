b5d9c51b6a97b64d54e3419125e30455ccd4e31b
Hari Dev <hariharidev5@gmail.com>
2024-09-06 13:03:28 +0530

    Hover Navbar ::after

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = ()=>{
           <div className="nav-title">DTC</div>
         <nav>
           <ul className="nav-ul">
-            <div className="nav-item"><li><a href="#">Home</a></li></div>
-            <div className="nav-item"><li ><a href="#">Routes</a></li></div>
-            <div className="nav-item"> <li><a href="#">Ticketing</a></li></div>
-            <div className="nav-item"><li><a href="#">Queries</a></li></div>
-            <div className="nav-item"><li><a href="#">Contact</a></li></div>
+            <div className="nav-item"><li><a className="nav-link" href="#">Home</a></li></div>
+            <div className="nav-item"><li ><a className="nav-link" href="#">Routes</a></li></div>
+            <div className="nav-item"> <li><a className="nav-link" href="#">Ticketing</a></li></div>
+            <div className="nav-item"><li><a  className="nav-link"href="#">Queries</a></li></div>
+            <div className="nav-item"><li><a className="nav-link"href="#">Contact</a></li></div>
             
           </ul>
           </nav>
